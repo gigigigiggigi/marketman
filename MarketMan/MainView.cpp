381e2e21d2e9d97013e5f417a02c690ea89ccb21
qweqaz12 <42571428+qweqaz12@users.noreply.github.com>
2020-04-11 00:40:12 +0800

    Update MainView.cpp

--- MarketMan/MainView.cpp
+++ MarketMan/MainView.cpp
@@ -47,6 +47,13 @@ void MainView::SqlLink()
      }
 }
 
+void MainView::handlelineeditclicked()
+{
+    float p = ui->lineEdit_stock_goodnum->text().toInt()*ui->lineEdit_stock_singleprice->text().toFloat();
+    QString data = QString("%1").arg(p);
+    ui->lineEdit_stock_totalprice->setText(data);
+}
+
 
 
 void MainView::SetListItem()
@@ -66,7 +73,7 @@ void MainView::InitGoodadd()
 
     QSqlQueryModel *model2 = new QSqlQueryModel(this);
     model2->setQuery(QString("select suppliers_name from suppliers"),db);
-    ui->cboc_goodadd_supllier->setModel(model2);
+    ui->cboc_goodadd_supplier->setModel(model2);
 
     QRegExp regx("^([0]|[1-9][0-9]{0,99})(?:\\.\\d{1,4})?$|(^\\t?$)");
     QValidator *validator = new QRegExpValidator(regx, ui->lineEdit_goodadd_id);
@@ -80,7 +87,59 @@ void MainView::InitGoodadd()
     ui->lineEdit_goodadd_inprice->setValidator(validator);
     ui->lineEdit_goodadd_outprice->setValidator(validator);
     ui->lineEdit_goodadd_pricecut->setValidator(validator);
+    ui->lineEdit_goodadd_cutstarttime->setDate(QDate::currentDate());
+    ui->lineEdit_goodadd_cutstoptime->setDate(QDate::currentDate());
 
+}
+
+void MainView::InitStockman_stockinput()
+{
+    QSqlQueryModel *model = new QSqlQueryModel(this);
+    model->setQuery(QString("select good_id from goods"),db);
+    ui->cbox_stock_goodid->setModel(model);
+    QString str = ui->cbox_stock_goodid->currentText();
+    QSqlQuery query(db);
+    QString sql = QString("select goods_name,goods_inprice from goods where good_id = '%1'").arg(str);
+    query.exec(sql);
+    while(query.next())
+    {
+        ui->lineEdit_stock_goodname->setText(query.value(0).toString());
+        ui->lineEdit_stock_singleprice->setText(query.value(1).toString());
+    }
+     ui->lineEdit_stock_goodname->setEnabled(false);
+     ui->lineEdit_stock_singleprice->setEnabled(false);
+
+    QSqlQueryModel *model2 = new QSqlQueryModel(this);
+    model2->setQuery(QString("select suppliers_name from suppliers"),db);
+    ui->cbox_stock_supplier->setModel(model2);
+    str = ui->cbox_stock_supplier->currentText();
+    sql = QString("select suppliers_phone from suppliers where suppliers_name = '%1'").arg(str);
+    query.exec(sql);
+    while(query.next())
+    {
+        ui->lineEdit_stock_supplierphone->setText(query.value(0).toString());
+    }
+     ui->lineEdit_stock_supplierphone->setEnabled(false);
+
+    QSqlQueryModel *model3 = new QSqlQueryModel(this);
+    model3->setQuery(QString("select user_name from user"),db);
+    ui->cbox_stock_person->setModel(model3);
+
+    QRegExp regx("^([0]|[1-9][0-9]{0,99})(?:\\.\\d{1,4})?$|(^\\t?$)");
+    QValidator *validator = new QRegExpValidator(regx, ui->lineEdit_goodadd_id);
+
+    ui->lineEdit_stock_goodnum->setValidator(validator);
+    ui->lineEdit_stock_totalprice->setValidator(validator);
+    ui->dateEdit_stock_time->setDate(QDate::currentDate());
+    connect(ui->lineEdit_stock_totalprice, SIGNAL(clicked()), this, SLOT(handlelineeditclicked()));
+
+}
+
+void MainView::InitSupplieradd()
+{
+    QRegExp regx("^([0]|[1-9][0-9]{0,99})(?:\\.\\d{1,4})?$|(^\\t?$)");
+    QValidator *validator = new QRegExpValidator(regx, ui->lineEdit_goodadd_id);
+    ui->lineEdit_stock_supplier_phone->setValidator(validator);
 }
 
 
@@ -140,6 +199,61 @@ void MainView::SetGoodsInfoTable()
 
 }
 
+void MainView::SetStockInfoTable()
+{
+    pModel_tablestockinfo = new intable_model(this,db);
+    pModel_tablestockinfo->setTable("stock");
+    pModel_tablestockinfo->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    pModel_tablestockinfo->select(); //选取整个表的所有行
+
+    pModel_tablestockinfo->setHeaderData(1, Qt::Horizontal, tr("商品编号"));
+    pModel_tablestockinfo->setHeaderData(2, Qt::Horizontal, tr("商品名称"));
+    pModel_tablestockinfo->setHeaderData(3, Qt::Horizontal, tr("商品数量"));
+    pModel_tablestockinfo->setHeaderData(4, Qt::Horizontal, tr("商品进价"));
+    pModel_tablestockinfo->setHeaderData(5, Qt::Horizontal, tr("商品总价"));
+    pModel_tablestockinfo->setHeaderData(6, Qt::Horizontal, tr("供货商"));
+    pModel_tablestockinfo->setHeaderData(7, Qt::Horizontal, tr("供货商电话"));
+    pModel_tablestockinfo->setHeaderData(8, Qt::Horizontal, tr("入库时间"));
+    pModel_tablestockinfo->setHeaderData(9, Qt::Horizontal, tr("入库工作人员"));
+
+    ui->tablestockinfo->setModel(pModel_tablestockinfo);
+    ui->tablestockinfo->setColumnHidden(0,true);
+    ui->tablestockinfo->setSelectionBehavior(QAbstractItemView::SelectRows);//设置选中模式为选中行
+    ui->tablestockinfo->show();
+
+
+}
+
+void MainView::SetSuppliersInfoTable()
+{
+    pModel_tablesuppliersinfo = new intable_model(this,db);
+    pModel_tablesuppliersinfo->setTable("suppliers");
+    pModel_tablesuppliersinfo->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    pModel_tablesuppliersinfo->select(); //选取整个表的所有行
+
+    pModel_tablesuppliersinfo->setHeaderData(1, Qt::Horizontal, tr("供货商名"));
+    pModel_tablesuppliersinfo->setHeaderData(2, Qt::Horizontal, tr("供货商电话"));
+    pModel_tablesuppliersinfo->setHeaderData(3, Qt::Horizontal, tr("供货商地址"));
+
+    ui->tablesuppliersinfo->setModel(pModel_tablesuppliersinfo);
+    ui->tablesuppliersinfo->setColumnHidden(0,true);
+    ui->tablesuppliersinfo->setSelectionBehavior(QAbstractItemView::SelectRows);//设置选中模式为选中行
+    ui->tablesuppliersinfo->show();
+
+
+
+
+}
+
+void MainView::SetModels()
+{
+    SetGoodsTypeTable();
+    SetGoodsInfoTable();
+    SetStockInfoTable();
+    SetSuppliersInfoTable();
+}
+
+
 void MainView::on_listView_pressed(const QModelIndex &index)
 {
 
@@ -157,7 +271,39 @@ void MainView::on_listView_pressed(const QModelIndex &index)
         InitGoodadd();
     }
     else if(str == "商品信息")
-        this->ui->stackedWidget->setCurrentIndex(2);
+    {
+         this->ui->stackedWidget->setCurrentIndex(2);
+
+    }
+    else if(str == "进货入库")
+    {
+        this->ui->stackedWidget->setCurrentIndex(4);
+        InitStockman_stockinput();
+    }
+    else if(str == "入库查询")
+    {
+        this->ui->stackedWidget->setCurrentIndex(5);
+        this->ui->btnupdate_stock->setVisible(false);
+    }
+    else if(str == "管理供货")
+    {
+        this->ui->stackedWidget->setCurrentIndex(6);
+    }
+    else if(str == "员工录入")
+    {
+        this->ui->stackedWidget->setCurrentIndex(7);
+    }
+    else if(str == "员工查询")
+    {
+        this->ui->stackedWidget->setCurrentIndex(8);
+    }
+    else if(str == "员工类型管理")
+    {
+        this->ui->stackedWidget->setCurrentIndex(9);
+    }
+
+
+
 
 }
 
@@ -230,11 +376,6 @@ void MainView::on_btndelete_goodtype_clicked()
 
 }
 
-void MainView::SetModels()
-{
-    SetGoodsTypeTable();
-    SetGoodsInfoTable();
-}
 
 void MainView::on_btnupdate_goodtype_clicked()
 {
@@ -249,6 +390,7 @@ void MainView::on_btnupdate_goodtype_clicked()
         if(b)
         {
             QMessageBox::information(this,"提示","修改成功");
+            pModel_tablegoodinfo->select();
 
         }
         else
@@ -329,7 +471,7 @@ void MainView::on_btnadd_goodadd_clicked()
    int good_onground = ui->lineEdit_goodadd_onground->text().toInt();
    int good_saled = ui->lineEdit_goodadd_saled->text().toInt();
    QString good_producer = ui->lineEdit_goodadd_producer->text();
-   QString good_supllier = ui->cboc_goodadd_supllier->currentText();
+   QString good_supllier = ui->cboc_goodadd_supplier->currentText();
    int good_warnline = ui->lineEdit_goodadd_warnline->text().toInt();
    QString pic = "";
 
@@ -346,7 +488,7 @@ void MainView::on_btnadd_goodadd_clicked()
    QSqlQuery query2 = QSqlQuery(db);
    QString sql  = QString("insert into goods values('%1','%2','%3','%4','%5','%6','%7','%8','%9','%10','%11','%12','%13','%14','%15','%16')").arg(good_id)
            .arg(good_barcode).arg(good_name).arg(good_type).arg(pic).arg(good_inprice)
-           .arg(good_outprice).arg(good_pricecut).arg(good_cutstarttime.toString()).arg(good_cutstoptime.toString())
+           .arg(good_outprice).arg(good_pricecut).arg(good_cutstarttime.toString("yy-MM-dd")).arg(good_cutstoptime.toString("yy-MM-dd"))
            .arg(good_onground).arg(good_saled).arg(good_warnline).arg(good_stock).arg(good_producer).arg(good_supllier);
    QString sql2  = QString("select good_barcode from goods where good_barcode = '%1'").arg(good_barcode);
    query2.exec(sql2);
@@ -379,12 +521,212 @@ void MainView::on_btnadd_goodadd_clicked()
          qDebug()<<db.lastError();
    }
 
+}
 
+void MainView::on_cbox_stock_goodid_currentIndexChanged(const QString &arg1)
+{
+    QSqlQuery query(db);
+    QString sql = QString("select goods_name,goods_inprice from goods where good_id = '%1'").arg(arg1);
+    query.exec(sql);
+    while(query.next())
+    {
+        ui->lineEdit_stock_goodname->setText(query.value(0).toString());
+         ui->lineEdit_stock_singleprice->setText(query.value(1).toString());
+    }
+}
 
+void MainView::on_btnadd_stock_clicked()
+{
+      QString good_id = ui->cbox_stock_goodid->currentText();
+      QString good_name = ui->lineEdit_stock_goodname->text();
+      QString good_num = ui->lineEdit_stock_goodnum->text();
+      QString good_singleprice = ui->lineEdit_stock_singleprice->text();
+      QString supplierphone = ui->lineEdit_stock_supplierphone->text();
+      QString totalprice = ui->lineEdit_stock_totalprice->text();
+      QString suppliername = ui->cbox_stock_supplier->currentText();
+      QString purchasername = ui->cbox_stock_person->currentText();
+      QDate data = ui->dateEdit_stock_time->date();
 
+      if(good_num == NULL||good_singleprice==NULL||totalprice==NULL)
+      {
+          QMessageBox::information(this,"提示","输入无效,检查空值！！！");
+          return;
+      }
 
+      QSqlQuery query = QSqlQuery(db);
+      QSqlQuery query2 = QSqlQuery(db);
 
+      QString sql1 = QString("update goods set goods_stocknum = goods_stocknum+'%1' where good_id = '%2' ").arg(good_num.toInt()).arg(good_id.toInt());
+      query.exec(sql1);
+      QString sql2 = QString("insert into stock(stock_goodid,stock_goodname,stock_goodnum,stock_goodsingleprice,stock_totalprice,stock_suppliername,stock_supplierphone,stock_stocktime,stock_purchaser) values('%1','%2','%3','%4','%5','%6','%7','%8','%9')")
+                     .arg(good_id).arg(good_name).arg(good_num).arg(good_singleprice).arg(totalprice).arg(suppliername).arg(supplierphone).arg(data.toString("yy-MM-dd")).arg(purchasername);
 
+      QString sd = data.toString();
+     bool b =  query2.exec(sql2);
+     if(b)
+     {
+          QMessageBox::information(this,"提示","添加成功");
+          ui->lineEdit_stock_goodnum->setText("");
+          ui->lineEdit_stock_totalprice->setText("");
+          pModel_tablestockinfo->select();
+          pModel_tablegoodinfo->select();
+     }
+     else
+     {
+         QMessageBox::information(this,"提示","失败");
+         qDebug()<<db.lastError();
+     }
+
+}
+
+void MainView::on_btndelete_stock_clicked()
+{
+    QItemSelectionModel *sModel = ui->tablestockinfo->selectionModel();
+
+    //取出模型中的索引
+    QModelIndexList list = sModel->selectedRows();
+    //删除所有选中的行
+    bool b;
+     qDebug()<<"com in";
+    if(list.size()>0)
+    {
+        for(int i=0; i < list.size(); i++){
+            pModel_tablestockinfo->removeRow( list.at(i).row() );
+        }
+
+        int ok = QMessageBox::warning(this,tr("删除当前行!"),tr("你确定删除当前行吗？"), QMessageBox::Yes,QMessageBox::No);
+
+        if(ok == QMessageBox::Yes)
+        {
+            qDebug()<<"com in";
+            pModel_tablestockinfo->submitAll();
+            b =  pModel_tablestockinfo->database().commit();
+            if(b)
+            {
+                QMessageBox::information(this,"提示","删除成功");
+
+            }
+            else
+                QMessageBox::information(this,"提示","失败");
+        }
+        else
+        {
+            pModel_tablestockinfo->revertAll();
+        }
+    }
+}
+
+void MainView::on_btnupdate_stock_clicked()
+{
+    int ok = QMessageBox::warning(this,tr("提交修改!"),tr("你确定提交修改吗？"), QMessageBox::Yes,QMessageBox::No);
+
+    if(ok == QMessageBox::Yes)
+    {
+        qDebug()<<"com in";
+        pModel_tablestockinfo->submitAll();
+        bool b;
+        b =  pModel_tablestockinfo->database().commit();
+        if(b)
+        {
+            QMessageBox::information(this,"提示","修改成功");
+        }
+        else
+            QMessageBox::information(this,"提示","失败");
+    }
+}
+
+void MainView::on_btndelete_stock_supplier_clicked()
+{
+    QItemSelectionModel *sModel = ui->tablesuppliersinfo->selectionModel();
+
+    //取出模型中的索引
+    QModelIndexList list = sModel->selectedRows();
+    //删除所有选中的行
+    bool b;
+     qDebug()<<"com in";
+    if(list.size()>0)
+    {
+        for(int i=0; i < list.size(); i++){
+            pModel_tablesuppliersinfo->removeRow( list.at(i).row() );
+        }
+
+        int ok = QMessageBox::warning(this,tr("删除当前行!"),tr("你确定删除当前行吗？"), QMessageBox::Yes,QMessageBox::No);
+
+        if(ok == QMessageBox::Yes)
+        {
+            qDebug()<<"com in";
+            pModel_tablesuppliersinfo->submitAll();
+            b =  pModel_tablesuppliersinfo->database().commit();
+            if(b)
+            {
+                QMessageBox::information(this,"提示","删除成功");
+
+            }
+            else
+                QMessageBox::information(this,"提示","失败");
+        }
+        else
+        {
+            pModel_tablesuppliersinfo->revertAll();
+        }
+    }
+}
+
+void MainView::on_btnupdate_stock_supplier_clicked()
+{
+    int ok = QMessageBox::warning(this,tr("提交修改!"),tr("你确定提交修改吗？"), QMessageBox::Yes,QMessageBox::No);
+
+    if(ok == QMessageBox::Yes)
+    {
+        qDebug()<<"com in";
+        pModel_tablesuppliersinfo->submitAll();
+        bool b;
+        b =  pModel_tablesuppliersinfo->database().commit();
+        if(b)
+        {
+            QMessageBox::information(this,"提示","修改成功");
+
+        }
+        else
+            QMessageBox::information(this,"提示","失败");
+    }
+}
+
+void MainView::on_btnadd_stock_supplier_clicked()
+{
+    QString suppliername = ui->lineEdit_stock_supplier_name->text();
+    QString supplierphone = ui->lineEdit_stock_supplier_phone->text();
+    QString supplieraddress = ui->lineEdit_stock_supplier_address->text();
+    if(supplieraddress==NULL||suppliername==NULL||supplierphone==NULL)
+    {
+        QMessageBox::information(this,"提示","输入无效,检查空值！！！");
+        return;
+    }
+    QSqlQuery query(db);
+    QString sql = QString("select suppliers_name from suppliers where suppliers_name = '%1'").arg(suppliername);
+    query.exec(sql);
+    while(query.next())
+    {
+        QMessageBox::information(this,"提示","重复添加");
+        return;
+    }
+
+    QString sql2 = QString("insert into suppliers(suppliers_name,suppliers_phone,suppliers_address) values('%1','%2','%3')")
+                    .arg(suppliername).arg(supplierphone).arg(supplieraddress);
+    bool b = query.exec(sql2);
+    if(b)
+    {
+         QMessageBox::information(this,"提示","添加成功");
+         ui->lineEdit_stock_supplier_address->setText("");
+         ui->lineEdit_stock_supplier_name->setText("");
+         ui->lineEdit_stock_supplier_phone->setText("");
+         pModel_tablesuppliersinfo->select();
+    }
+    else
+    {
+        QMessageBox::information(this,"提示","失败");
+        qDebug()<<db.lastError();
+    }
 
 
 }
